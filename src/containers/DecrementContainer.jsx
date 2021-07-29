71ffb9d62a88ab2f8f6d537c1445dd4ba0e33a03
otis wilcox <otiswilcox@gmail.com>
2021-07-29 13:42:32 -0400

    pushing up my react redux counter practice

---
+++ src/containers/DecrementContainer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { actionDecrement } from '../actions';
+
+const Decrement = props => (
+    <button onClick={props.handleClick}>-</button>
+);
+
+function mapDispatchToProps(dispatch) {
+    return {
+        handleClick: () => {
+            dispatch(actionDecrement());
+        }
+    }
+}
+
+export default connect(null, mapDispatchToProps)(Decrement);
---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { createStore } from 'redux';
+import { counter } from './reducers';
+// default state
+const defaultState = {
+    count: 42
+
+};
+
+export const store = createStore(counter, defaultState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
